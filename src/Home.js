29eaadcd8b46db384c97f01465b3ceba3bf84b5e
Harshit laxkar <hlaxkar@gmail.com>
2023-04-17 22:21:33 +0530

    handleDelete

--- src/Home.js
+++ src/Home.js
@@ -2,18 +2,19 @@ import { useState } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
 const [blogs, setBlog]= useState([
-    {title:'My new Website', body:'lorem impsum....', author: 'Mario',id:1},
-    {title:'Welcome Party!', body:'lorem impsum....', author: 'Luigi',id:2},
-    {title:'Web dev app tips', body:'lorem impsum....', author: 'Mario',id:3},
+    {title:'My new Website', body:'lorem impsum....', author: 'Duck',id:1},
+    {title:'Welcome Party!', body:'lorem impsum....', author: 'Akira',id:2},
+    {title:'Web dev app tips', body:'lorem impsum....', author: 'Duck',id:3},
 ])
     
     
-
+function handleDelete(id){ 
+    setBlog(blogs.filter((blog) => blog.id !== id)) }
     return (  
         <div className="home">
       <BlogList blogs={blogs} title='All Blogs'/>
-      <BlogList blogs={blogs} title='Duck Blogs'/>
-      <BlogList blogs={blogs} title='Akira Blogs'/>
+      <BlogList blogs={blogs.filter((blog) =>  {return blog.author==='Duck'} )} title='Duck Blogs' handleDelete={handleDelete} />
+      <BlogList blogs={blogs.filter((blog) => {return blog.author==='Akira' })} title='Akira Blogs' handleDelete={handleDelete} />
       
       
     </div>
